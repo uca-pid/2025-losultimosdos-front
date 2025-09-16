1a36b75bc9d9afed9be54964485a589136161608
Juan Francsico Seijas <panchoseijas@gmail.com>
2025-09-16 12:19:46 -0300

    login and signup components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 
 export default function App() {
-  return <Button>Click me</Button>;
+  return <Button>HOLA</Button>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import { ClerkProvider, SignIn } from "@clerk/react-router";
+import { ClerkProvider } from "@clerk/react-router";
 import "./index.css";
 import App from "./App.tsx";
+import LoginPage from "./pages/login-page.tsx";
+import { esMX } from "@clerk/localizations";
+import SignUpPage from "./pages/signup-page.tsx";
+import VerifyEmailPage from "./pages/verify-email.tsx";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -15,10 +19,15 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={esMX}>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/login" element={<SignIn />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route
+            path="/sign-up/verify-email-address"
+            element={<VerifyEmailPage />}
+          />
         </Routes>
       </ClerkProvider>
     </BrowserRouter>
