1494f5539c78c90362e32be3f5f6643de808c4da
Juan Francsico Seijas <panchoseijas@gmail.com>
2025-09-18 17:16:53 -0300

    minor changes

--- src/pages/signup-page.tsx
+++ src/pages/signup-page.tsx
@@ -4,7 +4,14 @@ import { SignUp, useSignIn } from "@clerk/react-router";
 export default function SignUpPage() {
   const { isLoaded } = useSignIn();
   return (
-    <div className="grid min-h-svh lg:grid-cols-2 bg-gradient-to-br from-sky-200 via-white to-orange-100 animate-gradient-slow">
+    <div
+      className={[
+        "grid min-h-svh lg:grid-cols-2",
+        "bg-gradient-to-br from-sky-200 via-white to-orange-100",
+        "dark:bg-gradient-to-r dark:from-[#2A7B9B] dark:via-[#57C785] dark:to-[#EDDD53]",
+        "animate-gradient-slow",
+      ].join(" ")}
+    >
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-md flex items-center justify-center">
